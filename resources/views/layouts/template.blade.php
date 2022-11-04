c70c3fec94e7be15f67a1c65514dcb4e088f436e
naxgvK2K9MYrjd4 <ricardoemanuelracca@gmail.com>
2022-11-04 11:12:09 -0300

    se agrego nuevo menu

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -6,15 +6,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <title>@yield('title')</title>
+    <!-- FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!-- Fonts -->
     <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
     <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
     <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous">
     </script>
+
     @livewireStyles
 
     <!-- Styles -->
@@ -28,10 +29,13 @@
     </style>
 </head>
 
-<body class="container antialiased">
+<body class="container">
 
     <header class="container">
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand d-inline-block w-25" href="{{ url('/') }}"><img src="../../images/logo.webp" class="img-thumbnail"></a>
+                </nav>
+        <!-- <nav class="navbar navbar-expand-lg navbar-light bg-light">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">Mauri Computacion</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
@@ -104,9 +108,51 @@
                     </form>
                 </div>
             </div>
-        </nav>
-    </header>
+        </nav> -->
+        <button class="btn btn-primary mt-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"><i class="fa-solid fa-bars"></i></button>
 
+        <div class="offcanvas offcanvas-start show w-25" data-bs-scroll="true" data-bs-backdrop="false" tabindex="-1" id="offcanvasScrolling" aria-labelledby="offcanvasScrollingLabel">
+            <div class="offcanvas-header">
+                <!-- <h5 class="offcanvas-title" id="offcanvasScrollingLabel">Titulo</h5> -->
+                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+            </div>
+            <div class="offcanvas-body">
+
+
+
+
+
+                        <div class="accordion-body">
+                            <!--List group-->
+                            <div class="list-group">
+                                <a href="{{route('clientes.index')}}" class="list-group-item list-group-item-action active" aria-current="true">
+                                <i class="fa-solid fa-user me-3"></i> CLIENTES
+                                </a>
+                                <a href="{{route('productos.index')}}" class="list-group-item list-group-item-action active mt-2" aria-current="true">
+                                <i class="fa-solid fa-laptop me-3"></i> PRODUCTOS
+                                </a>
+                                <a href="{{route('proveedores.index')}}" class="list-group-item list-group-item-action active mt-2" aria-current="true">
+                                <i class="fa-solid fa-handshake me-3"></i> PROVEEDORES
+                                </a>
+                                <a href="{{route('fabricantes.index')}}" class="list-group-item list-group-item-action active mt-2" aria-current="true">
+                                <i class="fa-solid fa-user me-3"></i>  FABRICANTES
+                                </a>
+                                <a href="{{route('categorias.index')}}" class="list-group-item list-group-item-action active mt-2" aria-current="true">
+                                   <i class="fa-solid fa-square-minus me-3"></i> CATEGORIAS
+                                </a>
+                                <a href="{{route('subcategorias.index')}}" class="list-group-item list-group-item-action active mt-2" aria-current="true">
+                                   <i class="fa-solid fa-square-minus me-3"></i> SUBCATEGORIAS
+                                </a>
+
+                            </div>
+                            <!--END List group-->
+                        </div>
+
+
+                <!-- End body offcanvas-->
+            </div>
+        </div>
+    </header>
     @yield('content')
 
 
